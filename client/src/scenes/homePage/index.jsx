49d22e2fffa589488706244ad446f876812fad82
Navdeep Singh <navdeep5@ualberta.ca>
2024-05-26 11:47:14 -0600

    See and interact with posts

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -4,6 +4,7 @@ import { Box, useMediaQuery } from "@mui/material";
 import { useSelector } from "react-redux";
 import UserWidget from "scenes/widgets/UserWidget";
 import MyPostWidget from "scenes/widgets/MyPostWidget";
+import PostsWidget from "scenes/widgets/PostsWidget";
 
 const HomePage = () => {
     const isNotMobileScreen = useMediaQuery("(min-width:1000px)");
@@ -26,6 +27,7 @@ const HomePage = () => {
                      mt={isNotMobileScreen ? undefined : "2rem"}
                 >
                     <MyPostWidget picturePath={picturePath} />
+                    <PostsWidget userId={_id} />
                 </Box>
                 {isNotMobileScreen && (
                     <Box flexBasis="26%">
